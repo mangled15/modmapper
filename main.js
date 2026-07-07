b0a290fac4dd80c6608faff834c27b89313568bf
Mangled <149960279+mangled15@users.noreply.github.com>
2026-07-07 07:54:37 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 /**
  * Animates a note | Recommended to use with a for loop
+ * @param {any} diffJSON - JSON data of the difficulty
  * @param {number} beat - The beat the note is on that you want to animate
  * @param {{
  * animation?: {
+ * spawnEffect: boolean
  * offsetPosition: [][]
  * rotation: [][]
  * scale?: [][]
  * }}} properties - Properties for the animation
  */
-export function animateNote(beat, properties) {
-    if (json.colorNotes.forEach(note => {
+export function animateNote(diffJSON, beat, properties) {
+    if (diffJSON.colorNotes.forEach(note => {
         if(note.b == beat) {
             note.customData = properties
         }
@@ -21,6 +23,7 @@ export function animateNote(beat, properties) {
 
 /**
  * Animates a set of notes in between 2 beats(INCLUSIVE) using a for loop
+ * @param {any} diffJSON - JSON data of the difficulty
  * @param {number} beat1 - The start beat
  * @param {number} beat2 - The end beat
  * @param {{
@@ -31,9 +34,9 @@ export function animateNote(beat, properties) {
  * scale?: [][]
  * }}} properties - Properties for the animation
  */
-export function animateNoteInBetween(beat1, beat2, properties) {
+export function animateNoteInBetween(diffJSON, beat1, beat2, properties) {
     if (properties) {
-        for (const note of json.colorNotes) {
+        for (const note of diffJSON.colorNotes) {
             if (note.b >= beat1 && note.b <= beat2){
                 note.customData = properties
             }
