f3699f82594fb7944652c0d8ce9035be8a8bd6d9
Mangled <149960279+mangled15@users.noreply.github.com>
2026-07-07 07:33:18 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-const fs = require("fs");
-
-if (fs.existsSync("ExpertPlusStandard.dat")) {
-    console.log("\x1b[32m%s\x1b[0m", "[OK] ExpertPlusStandard.dat found");
-}
-else {
-    console.log("\x1b[31m%s\x1b[0m", "[CRITICAL] ExpertPlusStandard.dat not found");
-}
-
-const diff = "ExpertPlusStandard.dat"
-
-let data = fs.readFileSync(diff, "utf-8");
-
-let json = JSON.parse(data);
-
 /**
  * Animates a note | Recommended to use with a for loop
  * @param {number} beat - The beat the note is on that you want to animate
@@ -55,10 +40,7 @@ export function animateNoteInBetween(beat1, beat2, properties) {
         }
     }
 }
-console.log("\x1b[33m%s\x1b[0m", "[WARNING] writing to file...");
 fs.writeFileSync(
     diff,
     JSON.stringify(json, null, 4)
 );
-
-console.log("\x1b[32m%s\x1b[0m", "[OK] Succesfully executed the script");
