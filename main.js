5cbd11b27a61bb857d0754c64301663dbc26da19
Mangled <149960279+mangled15@users.noreply.github.com>
2026-07-08 12:29:54 -0700

    Added light event stuff + more debugging

--- main.js
+++ main.js
@@ -43,8 +43,17 @@ export async function setDifficulty(diff) {
     if (found) {
         data = fs.readFileSync(difficulty, "utf-8")
         diffJSON = JSON.parse(data)
+
+        // erasing previous custom data
         diffJSON.customData = {}
         diffJSON.customData.customEvents = []
+        diffJSON.customData.environment = []
+        diffJSON.colorNotes.forEach(note => {
+            delete note.customData
+        })
+        diffJSON.basicBeatmapEvents.forEach(event => {
+            delete event.customData
+        })
     }
     else {
         console.log(colors.magenta, `[CRITICAL] Could not find ${difficulty} in the current directonary`, colors.reset)
@@ -52,7 +61,7 @@ export async function setDifficulty(diff) {
     }
 }
 
-// -----------------------------------------------------------------------------------------------------
+// --- NOTES ---
 /**
  * @typedef {Object} noteAnimationProperties
  * @property {boolean} [spawnEffect] - Should the note have a spawn effect? Either true or false. Default = true
@@ -88,9 +97,6 @@ export function animateNote(beat, properties) {
         if (note.b == beat) {
             note.customData = properties
         }
-        else {
-            console.log(`note on beat ${beat} not found`)
-        }
     }));
 }
 
@@ -109,6 +115,8 @@ export function animateNoteInBetween(beat1, beat2, properties) {
         }
     }
 }
+
+// --- TRACKS ---
 
 /**
  * @typedef {Object} animateTrackProperties
@@ -157,23 +165,100 @@ export function animateTrack(properties) {
     }
 }
 
+// --- ENVIRONMENT ---
+
+/**
+ * @typedef {"Regex"|"Exact"|"Contains"|"StartsWith"|"EndsWith"} LookupMethod
+ */
+
+/**
+ * @typedef {Object} modifyEnvironmentProperties
+ * @property {string} id - Enter the id of the EnvironmentObject
+ * @property {LookupMethod} lookupMethod - What method should be used to look up the object? Regex, Exact, Contains, StartsWith, EndsWith. Default = Regex
+ * @property {number} [duplicate] - How many duplicates should be made of the object?
+ * @property {boolean} [active] - Should the object activated? | true or false. Default = true
+ * @property {any} [position] - Where should the object be positioned?
+ * @property {any} [rotation] - How should the object be rotated?
+ * @property {any} [scale] - How big should the object be?
+ * @property {any} [localPosition] - Where should the object be positioned relitive to it's set position?
+ * @property {any} [localRotation] - How should the object be rotated relative to it's set rotation??
+ * @property {any} [track] - What track should be assigned to the object?
+ * @property {{
+ *      ILightWithId?: {
+ *          lightID?: number
+ *          type?: number
+ *      }
+ *      BloomFogEnvironment?: {
+ *          attenuation?: number
+ *          startY?: number
+ *          height?: number
+ *      }
+ *      TubeBloomPrePassLight?: {
+ *          colorAlphaMultiplier?: number
+ *          bloomFogIntensityMultiplier?: number
+ *      }
+ * }} [components]
+ */
+
+/**
+ * Used to modify(NOT ANIMATE) objects in the environment. It can be pretty complicated. For more information and guides go to the modmapper wiki(Doesn't exist yet)
+ * @param {modifyEnvironmentProperties} properties
+ */
+export function modifyEnvironment(properties) {
+    if (diffJSON && properties) {
+        diffJSON.customData.environment.push(properties)
+    }
+}
+
+// --- LIGHTS ---
+
+/**
+ * @typedef {Object} lightEventProperties
+ * @property {number} [lightID] - What ID does the light event get?
+ * @property {number} [brightness] - How bright should the light be? 1 is default brightness but can go way above or below.
+ * @property {any} [color] - What color should the light be? You can set this is chromapper or manually do it here. Look at the wiki on how you should input colors.
+ * @property {string} [easing] - What easing style should the light event have?
+ * @property {string} [lerpType] - What lerping should the light even have? Either "RGB" or "HSV"
+ */
+
+/**
+ * Used to edit a light event at a certain beat, recommended to use with a for loop.
+ * @param {number} beat - At what beat should the light even take place?
+ * @param {lightEventProperties} properties - What properties should the light event have?
+ */
+export function editLightEvent(beat, properties) {
+    if (diffJSON && properties) {
+        diffJSON.basicBeatmapEvents.forEach(event => {
+            if (event.b == beat) {
+                event.f = properties.brightness
+                let newProperties = {
+                    lightID: properties.lightID,
+                    color: properties.color,
+                    easing: properties.easing,
+                    lerpType: properties.lerpType
+                }
+                event.customData = newProperties
+            }
+        })
+    }
+}
+
+// --- WRITING TO FILE ---
+
 /**
  * Writes the custom data to the file you have set with modmapper.setDifficulty
  */
 export function writeToFile() {
-    fileExists(difficulty).then((found) => {
-        if (diffJSON && found) {
-            console.log(colors.yellow, `[WARNING] writing to ${difficulty}...`, colors.reset)
-            fs.writeFileSync(
-                difficulty,
-                JSON.stringify(diffJSON, null, 4)
-            );
-
-            console.log(colors.green, "[OK] Succesfully written to " + difficulty, colors.reset)
-        }
-        else {
-            console.log(colors.red, `[ERROR] Error writing to ${difficulty}, did you set the correct difficulty name?`, colors.reset)
-            return
-        }
-    })
+    if (diffJSON) {
+        console.log(colors.yellow, `[WARNING] writing to ${difficulty}...`, colors.reset)
+        fs.writeFileSync(
+            difficulty,
+            JSON.stringify(diffJSON, null, 4)
+        );
+        console.log(colors.green, "[OK] Succesfully written to " + difficulty, colors.reset)
+    }
+    else {
+        console.log(colors.red, `[ERROR] Error writing to ${difficulty}, did you set the correct difficulty name?`, colors.reset)
+        return
+    }
 }
